40b3979e2c6e6118574e875755af16db972a0151
Edouard Lopez <dev+git@edouard-lopez.com>
2013-06-29 22:59:39 +0200

    Feat(installer): init script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# DESCRIPTION
+#   Install sshuttle-helper as an init-script
+#
+# USAGE
+#   curl -L https://github.com/edouard-lopez/sshuttle-helper/raw/master/install.sh | sh
+#
+# AUTHOR
+#   Édouard Lopez
+
