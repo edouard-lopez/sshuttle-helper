cbf521ef28aaec1173bca64a5db594133e65160e
Edouard Lopez <dev+git@edouard-lopez.com>
2013-06-29 23:35:15 +0200

    docs(installer): clone and copy to init directory
    Signed-off-by: Edouard Lopez <dev+git@edouard-lopez.com>

--- install.sh
+++ install.sh
@@ -8,10 +8,13 @@
 # AUTHOR
 #   Édouard Lopez
 
+# clone
 echo "\033[0;34mCloning SSHuttle-helper...\033[0m"
 hash git >/dev/null && /usr/bin/env git clone git://github.com/edouard-lopez/sshuttle-helper.git || {
   echo "git not installed"
   exit
 }
+
+# copy to System-V style init script directory
 cd sshuttle-helper
 cp ./proxy /etc/init.d/
