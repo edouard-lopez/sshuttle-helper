48a892ec297ce19f05ab21437b8fc759c87be83d
Edouard Lopez <dev+git@edouard-lopez.com>
2013-07-08 15:57:32 +0200

    fix(install.sh): test if init-script exists before copy
    Signed-off-by: Edouard Lopez <dev+git@edouard-lopez.com>

--- install.sh
+++ install.sh
@@ -14,7 +14,11 @@ hash git >/dev/null && /usr/bin/env git clone git://github.com/edouard-lopez/ssh
   echo "git not installed"
   exit
 }
+initDir=/etc/init.d
 
 # copy to System-V style init script directory
 cd sshuttle-helper
-sudo cp ./proxy-ssh /etc/init.d/
+if [ -e "$initDir/proxy-ssh" ]; then
+  echo "Adding init script..."
+  sudo cp ./proxy-ssh /etc/init.d/
+fi
